ab5db9992032214579f0b84868028471ade79f11
Kostas Bourantas <bourantas@ceid.upatras.gr>
2020-01-31 12:13:16 +0200

    Update progress-scraper.py

--- progress-scraper.py
+++ progress-scraper.py
@@ -7,10 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.options import Options
-from tqdm import *
 import sys
-
-import threading
 
 import time
 ##################################################################################################################
